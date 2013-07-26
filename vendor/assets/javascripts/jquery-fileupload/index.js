1d6e35d3ad22354cfc1d997544e6a4cb141bf6ff
I-Ta Tsai <ittsai@gmail.com>
2013-07-26 08:48:22 +0800

    follow the newest jquery-fileupload

--- vendor/assets/javascripts/jquery-fileupload/index.js
+++ vendor/assets/javascripts/jquery-fileupload/index.js
@@ -4,6 +4,9 @@
 //=require jquery-fileupload/vendor/tmpl
 //=require jquery-fileupload/jquery.iframe-transport
 //=require jquery-fileupload/jquery.fileupload
-//=require jquery-fileupload/jquery.fileupload-fp
+//=require jquery-fileupload/jquery.fileupload-process
+//=require jquery-fileupload/jquery.fileupload-image
+//=require jquery-fileupload/jquery.fileupload-audio
+//=require jquery-fileupload/jquery.fileupload-validate
 //=require jquery-fileupload/jquery.fileupload-ui
 //=require jquery-fileupload/locale
